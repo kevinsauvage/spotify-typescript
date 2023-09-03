708211c72ad258b308931c32c2061b233f10448d
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-09-03 21:54:27 +0200

    undo refactor

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,38 +1,26 @@
-/* eslint-disable no-param-reassign */
 import { RequestCookie } from 'next/dist/compiled/@edge-runtime/cookies';
 import { type NextRequest, NextResponse } from 'next/server';
 
-const noAccessToken = (request: NextRequest) => {
-  const { nextUrl } = request || {};
-  const { pathname } = nextUrl || {};
-
-  if (!pathname.startsWith('/login') || pathname.startsWith('/logout')) {
-    nextUrl.pathname = '/login';
-    return NextResponse.redirect(nextUrl);
-  }
-};
-
-const hasAccessToken = (request: NextRequest) => {
-  const { nextUrl } = request;
-  const { pathname } = nextUrl || {};
-
-  if (pathname.startsWith('/login')) {
-    nextUrl.pathname = '/';
-    return NextResponse.redirect(nextUrl);
-  }
-};
-
-const checkAccessToken = async (request: NextRequest) => {
+const middleware = async (request: NextRequest) => {
   const spotifyToken: RequestCookie | undefined = request.cookies.get('spotify_token');
+  const { nextUrl } = request;
+
   const spotifyTokenParsed = JSON.parse(spotifyToken?.value || '{}');
   const { accessToken } = spotifyTokenParsed || {};
 
-  if (accessToken) hasAccessToken(request);
-  if (!accessToken) noAccessToken(request);
-};
+  if (accessToken && nextUrl.pathname.startsWith('/login')) {
+    nextUrl.pathname = '/';
+    return NextResponse.redirect(nextUrl);
+  }
 
-const middleware = async (request: NextRequest) => {
-  checkAccessToken(request);
+  if (
+    !accessToken &&
+    (!nextUrl.pathname.startsWith('/login') || nextUrl.pathname.startsWith('/logout'))
+  ) {
+    nextUrl.pathname = '/login';
+    return NextResponse.redirect(nextUrl);
+  }
+
   return NextResponse.next();
 };
 
