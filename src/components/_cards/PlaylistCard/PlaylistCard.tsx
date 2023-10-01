293bcb351dbd2c0b5700c5642053b4bfc74707b6
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-10-01 22:51:56 +0200

    remove list item

--- src/components/_cards/PlaylistCard/PlaylistCard.tsx
+++ src/components/_cards/PlaylistCard/PlaylistCard.tsx
@@ -13,7 +13,7 @@ const PlaylistCard: React.FC<{
   const image = (images?.length && images?.at(1)) || images?.at(0) || images?.pop();
 
   return (
-    <li className={styles.playlist}>
+    <div className={styles.playlist}>
       {image?.url && <Image alt="Album cover" src={image?.url} width={200} height={200} />}
       <div className={styles.content}>
         <Link href={`/playlists/${id}`} className={styles.name}>
@@ -21,7 +21,7 @@ const PlaylistCard: React.FC<{
         </Link>
         <p className={styles.totalTracks}>{tracks?.total} tracks</p>
       </div>
-    </li>
+    </div>
   );
 };
 
