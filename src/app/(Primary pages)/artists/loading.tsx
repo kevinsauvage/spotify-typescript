217fbb79d21ad6fd56b6fc2bd34f9f022e2c1c45
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-10-08 22:38:05 +0200

    improve responsive

---
+++ src/app/(Primary pages)/artists/loading.tsx
@@ -0,0 +1,9 @@
+import ScreenLoader from '@/components/ScreenLoaderAbsolute/ScreenLoader';
+
+interface IProperties {}
+
+const Loading: React.FC<IProperties> = () => {
+  return <ScreenLoader />;
+};
+
+export default Loading;
