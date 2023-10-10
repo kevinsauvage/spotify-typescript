2c2636f354859d15503b2101391e785d7a356897
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-10-10 20:52:25 +0200

    add sitemap

---
+++ src/app/sitemap.xml/route.tsx
@@ -0,0 +1,42 @@
+const URL = 'https://spotify-typescript.vercel.app';
+
+function generateSiteMap(pages: { id: string }[]) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
+      <url>
+        <loc>${URL}</loc>
+      </url>
+      ${pages
+        ?.map(({ id }) => {
+          return `
+            <url>
+                <loc>${URL}/${id}</loc>
+            </url>
+            `;
+        })
+        .join('')}
+   </urlset>
+ `;
+}
+
+export async function GET() {
+  const pages = [
+    { id: 'artists' },
+    { id: 'traks' },
+    { id: 'playlists' },
+    { id: 'albums' },
+    { id: 'login' },
+    { id: 'search' },
+    { id: 'top-artists' },
+    { id: 'top-tracks' },
+  ];
+  const body = generateSiteMap(pages);
+
+  return new Response(body, {
+    headers: {
+      'Cache-control': 'public, s-maxage=86400, stale-while-revalidate',
+      'content-type': 'application/xml',
+    },
+    status: 200,
+  });
+}
